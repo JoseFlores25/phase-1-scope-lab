a564ff94dfa5bc7195b1e2fbc5671325471276bb
Jose <flores.jose2185@gmail.com>
2021-11-04 19:12:42 -0400

    Work is done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+
+const leastFavoriteCustomer = "Flores";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "No Allow Flores";
+  return leastFavoriteCustomer;
+}
